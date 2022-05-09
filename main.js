2b72d72806e5dc3f7932387ced7f47a488036806
brawl-kyan <79627900+brawl-kyan@users.noreply.github.com>
2022-05-09 16:20:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addUser(){
+    player1 = document.getElementById("player1_name").value;
+    player2 = document.getElementById("player2_name").value;
+    
+    localStorage.setItem("player1",player1);
+    localStorage.setItem("player2",player2);
+    
+    window.location ="game.html";
+    }
